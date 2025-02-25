acae552f1dc5dafac68c828a2a501b388c4626e7
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2025-02-26 00:21:14 +0100

    style: cleaning code

--- src/services/cocktails.ts
+++ src/services/cocktails.ts
@@ -3,9 +3,7 @@ const API_URL = "https://www.thecocktaildb.com/api/json/v1/1";
 export const fetchCocktail = async () => {
     try {
         const response = await fetch(`${API_URL}/random.php`);
-
         const data = await response.json();
-        console.log(data);
 
         return data;
     } catch (error) {
@@ -20,8 +18,6 @@ export const fetchCocktails = async (count: number = 3) => {
         const res = await fetchCocktail();
         const item = res?.drinks[0];
 
-        console.log(item, '--------');
-
         const ingredientsPattern = new RegExp(/strIngredient\d+/);
         const listIngrediens: any = [];
 
@@ -29,7 +25,6 @@ export const fetchCocktails = async (count: number = 3) => {
             if (ingredientsPattern.test(key) && item[key]) {
                 const ingredient = item[key];
                 listIngrediens.push(ingredient);
-                console.log(ingredient, '--------');
             }
         });
 
@@ -44,8 +39,6 @@ export const fetchCocktails = async (count: number = 3) => {
 
         cocktails.push(cocktail);
     }
-
-    console.log(cocktails, '--------');
 
     return cocktails;
 }
