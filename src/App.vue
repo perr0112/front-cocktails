82e52420efcbcb3b22a4da81eeba3a085ebb6522
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2025-02-26 00:21:35 +0100

    feat: loader added

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 import { fetchCocktails } from './services/cocktails';
-
 import cocktailcard from './components/CocktailCard.vue';
 
-const cocktails = ref();
+const cocktails = ref([]);
+const isLoading = ref(false);
+const isError = ref(false)
 
 const loadCocktails = async () => {
+  isLoading.value = true;
   try {
     const res = await fetchCocktails(3);
     cocktails.value = res;
+    await new Promise(resolve => setTimeout(resolve, 1000));
   } catch (error) {
-    console.error('Error fetching cocktails:', error);
+    isError = true
+  } finally {
+    isLoading.value = false;
   }
 }
 
-onMounted(async () => {
-  await loadCocktails();
-})
+onMounted(loadCocktails);
 </script>
 
 <template>
@@ -25,19 +28,28 @@ onMounted(async () => {
     <h1>🏖️ Beach Club</h1>
     <span>+ Menu</span>
   </header>
-  
+
   <div class="line" style="margin-top: 1rem;"></div>
 
   <container>
     <h3>Découvrez nos trois cocktails phares du moment !</h3>
-    <div class="btn">Charger trois nouveaux cocktails</div>
+    <div class="btn" @click="loadCocktails" :class="{ 'disabled': isLoading }">
+      {{ isLoading ? 'Chargement...' : 'Charger de nouveaux cocktails' }}
+    </div>
 
-    <div class="cocktails">
+
+    <div v-if="isError">Une erreur est survenue. Veuillez réessayer</div>
+
+    <div v-else class="cocktails">
+      <div v-if="isLoading">
+        <span class="loader"></span>
+      </div>
       <cocktailcard
+        v-else
         v-for="cocktail in cocktails"
+        :key="cocktail.id"
         :cocktail="cocktail"
-      >
-      </cocktailcard>
+      />
     </div>
   </container>
 </template>
